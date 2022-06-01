387689a3c6dd91d7e0001cb36e2f4af36ff71464
alamgir hossain <alamgir555hossain@gmail.com>
2022-05-31 23:02:25 -0400

    orders-serv

---
+++ orders/src/routes/new.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+import express, { Request, Response } from "express";
+import { requireAuth, vaildateRequest } from "@rktickets555/common";
+import { body } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketId must be provided"),
+  ],
+  vaildateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
+
+export { router as newOrderRouter };
