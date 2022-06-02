e2ae0c42dfb4b392cf08bef7e07a878cea25cdd3
alamgir hossain <alamgir555hossain@gmail.com>
2022-06-02 17:09:32 -0400

    fetches-order-particUser-func

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -8,8 +8,8 @@ import {
   BadRequestError,
 } from "@rktickets555/common";
 import { body } from "express-validator";
-import { Order } from "../models/order";
 import { Ticket } from "../models/ticket";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
@@ -29,14 +29,19 @@ router.post(
   async (req: Request, res: Response) => {
     const { ticketId } = req.body;
 
+    // Find the ticket the user is trying to order in the database
     const ticket = await Ticket.findById(ticketId);
     if (!ticket) {
       throw new NotFoundError();
     }
+
+    // Make sure that this ticket is not already reserved
     const isReserved = await ticket.isReserved();
     if (isReserved) {
       throw new BadRequestError("Ticket is already reserved");
     }
+
+    // Calculate an expiration date for this order
     const expiration = new Date();
     expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
