37f6d9ae59066c2e383bf67d27e32b79072f9369
alamgir hossain <alamgir555hossain@gmail.com>
2022-06-07 22:44:12 -0400

    reject-ticket-

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,6 +5,7 @@ import {
   vaildateRequest,
   requireAuth,
   NotAuthorizedError,
+  BadRequestError,
 } from "@rktickets555/common";
 import { Ticket } from "../models/ticket";
 import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
@@ -38,6 +39,10 @@ router.put(
     if (!ticket) {
       throw new NotFoundError();
     }
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit a reserved ticket");
+    }
+
     if (ticket.userId !== (<any>req).currentUser!.id) {
       throw new NotAuthorizedError();
     }
